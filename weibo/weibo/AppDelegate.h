0c07a068c3c69a1fe3d7097469e75b703bfbd1af
v_xueluo <v_xueluo@v-xueluodeimac.local>
2017-03-23 17:26:33 +0800

    初始化项目

---
+++ weibo/weibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weibo
+//
+//  Created by v_xueluo on 17/3/23.
+//  Copyright © 2017年 v_xueluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by v_xueluo on 17/3/23.
+//  Copyright © 2017年 v_xueluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
